76b87e3366c1015651c713f1fa9e917083b49572
karthiktatikonda24 <karthikreddy.aether@gmail.com>
2026-07-06 19:31:55 +0530

    initial clean commit

---
+++ week1/week1 practice/printinsamerow.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "cs50.h"
+
+int main (void)
+{
+    for ( int i = 1; i <=4 ; i++)
+    {
+        printf("?\n");// for printing question mark,in next line we use \n
+    }
+    printf("\n");// for printing the directory location in next line
+}
+
+//THANKYOU 
+//AUTHOR KARTHIK(IITPATNA)
