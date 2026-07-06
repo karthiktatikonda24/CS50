e21d46384f4596d58d9a2d050137eab8b1534295
karthiktatikonda24 <karthikreddy.aether@gmail.com>
2026-07-06 20:03:52 +0530

    initial clean commit

--- week1/week1 practice/mario.c
+++ week1/week1 practice/mario.c
@@ -3,12 +3,15 @@
 
 int main (void)
 {
-    for ( int i = 1; i <=4 ; i++)
+    for ( int row = 0 ; row < 3 ; row++)// first the compiler comes here and reads this and then goes to next line of code 
     {
-        printf("?\n");// for printing question mark,in next line we use \n
+        for ( int column = 0 ; column <3; column++) // then the compiler comes here and from row 1 it completes entier column 1 
+        {
+            printf("H");
+        }
+        printf("\n");// now the compiler comes here and prints the new line and then goes to next row and completes the entier column 2 and so on
     }
-    printf("\n");// for printing the directory location in next line
-}
+}// always remember only after completing the inner loop the compiler comes to the outer loop and then goes to next row and completes the entier column 2 and so on
 
 //THANKYOU 
 //AUTHOR KARTHIK(IITPATNA)
